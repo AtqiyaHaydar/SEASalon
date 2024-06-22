7a68dd11de99c01dcf3335428ff302ce9a5202ec
AtqiyaHaydar <ashford1524@gmail.com>
2024-06-22 20:47:30 +0700

    feat: initial components

---
+++ app/(home)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>SEA Salon, Beauty and Elegance Redefined</h1>
+      <p>
+        Experience top-notch treatments with a touch of elegance at our salon, where your beauty is enhanced and luxury is felt.
+      </p>
+    </div>
+  )
+}
+
+export default HomePage
