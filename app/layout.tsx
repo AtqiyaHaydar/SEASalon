431fd525ca898d6e899ec7fa1ce218d2fdbe06f2
AtqiyaHaydar <ashford1524@gmail.com>
2024-06-23 21:45:24 +0700

    feat: add poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <Header />
         {children}
         <Footer />
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header className='fixed w-full top-0 h-[80px] py-4 px-8 flex justify-between items-center ' data-aos="fade-down">
-      <h1  className='text-2xl'>
+      <h1  className='text-2xl poppins-regular'>
         SEA Salon
       </h1>
     </header>
