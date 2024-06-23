ed8ce66fdb824722e6aac49c9a71fb701208193b
AtqiyaHaydar <ashford1524@gmail.com>
2024-06-23 21:29:49 +0700

    feat: shadcn ui

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,13 +7,13 @@ import Aos from 'aos';
 const HomePage = () => {
   useEffect(() => {
     Aos.init({
-      duration: 1000, // Animation duration in milliseconds
-      once: true,     // Whether animation should happen only once - while scrolling down
+      duration: 1000,
+      once: true,
     });
   }, []);
 
   return (
-    <div data-aos="fade-up">
+    <div className='mt-[96px]'>
       <h1>SEA Salon, Beauty and Elegance Redefined</h1>
       <p>
         Experience top-notch treatments with a touch of elegance at our salon, where your beauty is enhanced and luxury is felt.
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='fixed w-full top-0 h-[80px] py-4 px-8 flex justify-between items-center bg-rose-200' data-aos="fade-down">
+      <h1  className='font-great-vibes'>
+        SEASalon
+      </h1>
+    </header>
   )
 }
 
