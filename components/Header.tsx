35dff90ae65f245bdfff35cc57aa061148516ac8
AtqiyaHaydar <ashford1524@gmail.com>
2024-06-24 13:18:57 +0700

    feat: style navbar add border when scrolling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
 
 import Logo from "@/public/Logo.svg"
 import Image from 'next/image'
@@ -8,8 +10,26 @@ import Link from 'next/link'
 import { cn } from '@/lib/utils'
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className='fixed w-full top-0 h-[80px] py-4 flex justify-between items-center' data-aos="fade-down">
+    <header className={`fixed z-20 w-full top-0 h-[80px] py-4 flex justify-between items-center transition-colors duration-300 ${scrolled ? 'bg-white/50 border-b border-gold backdrop-blur-md' : 'bg-transparent'}`} data-aos="fade-down">
       <div className='wrapper w-full flex justify-between items-center'>
         <div className='flex gap-x-8 items-center'>
           <Image 
--- components/Services.tsx
+++ components/Services.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div id="servicesection" className='wrapper'>
-      <h3 className='text-3xl font-alta'>Our Services</h3>
+    <div className='wrapper flex flex-col gap-y-12 items-center mt-16'>
+      <div className='text-center space-y-4'>
+        <h3 className='text-3xl font-alta text-gold font-bold' data-aos="fade-up">Our Services</h3>
+        <p className='text-black/50 text-center max-w-[750px]'>
+        Explore our premium beauty treatments, from luxurious facials to expert hair styling, tailored to rejuvenate and enhance your natural beauty.
+      </p>
+      </div>
+      <div>
+      awda
+      </div>
+      <div  id="servicesection" />
     </div>
   )
 }
